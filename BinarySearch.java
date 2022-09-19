e390b1ea31ac5b5dfcb83b36f9f4cf94360a100e
Shashank Bettadapura <106806734+shashankb87@users.noreply.github.com>
2022-09-19 12:42:59 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -3,21 +3,23 @@ import java.util.*;
 public class BinarySearch{
   //Find the target in a rotated sorted array
     public static int search(int[] nums, int target) {
-        int l = 0,r = nums.length-1;
-        while(r>=l){
-            int mid = l + (r-l)/2;
-            if(target == nums[mid])
-                return mid;
-            else if(nums[l] <= nums[mid]){
-                if(target < nums[mid] && target >= nums[l])
-                    r = mid-1;
-                else
-                    l = mid+1;
+        int l = 0, r = nums.length-1;
+        while(l<=r){
+            int m = l + (r-l)/2;
+            if(nums[m] == target)
+                return m;
+            if(nums[l] <= nums[m]){
+                if(target < nums[m] && target >= nums[l]){
+                    r = m - 1;
+                }else{
+                    l = m + 1;
+                }
             }else{
-                if(target > nums[mid] && target <= nums[r])
-                    l = mid + 1;
-                else
-                    r = mid - 1;
+                if(target > nums[m] && target <= nums[r]){
+                    l = m + 1;
+                }else{
+                    r = m - 1;
+                }
             }
         }
         return -1;

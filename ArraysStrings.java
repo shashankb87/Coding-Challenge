531b34d383e43410319889c6ca7af22c5fa3b1fb
Shashank Bettadapura <106806734+shashankb87@users.noreply.github.com>
2022-09-25 22:38:50 +0530

    Update ArraysStrings.java

--- ArraysStrings.java
+++ ArraysStrings.java
@@ -172,4 +172,28 @@ Only the filled cells need to be validated according to the mentioned rules.
         }
         return true;
     }
+    
+    /*  https://leetcode.com/problems/container-with-most-water/
+    You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+    */
+    public static int maxArea(int[] height) {
+        int maxA = 0;
+        int i = 0,j = height.length-1;
+        while(i<j){
+            int area = Math.min(height[i],height[j])*(j-i);
+            if(area > maxA)
+                maxA = area;
+            if(height[j] > height[i])
+                i++;
+            else
+                j--;
+        }
+        return max;
+    }
 }
